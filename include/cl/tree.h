f6a9a16632efe4b0fab229df2e2a4fe8cd7971db
Kate F <kate@elide.org>
2012-01-17 15:51:57 +0000

    Oops... this should have been in [20].

--- include/cl/tree.h
+++ include/cl/tree.h
@@ -33,7 +33,7 @@ struct cl_command {
 struct cl_tree *cl_create(size_t command_count, const struct cl_command commands[],
 	size_t field_count, const struct cl_field fields[],
 	int (*printprompt)(struct cl_peer *p, int mode),
-	int (*visible)(struct cl_peer *p, int mode, const struct cl_command *command),
+	int (*visible)(struct cl_peer *p, int mode, int modes),
 	int (*vprintf)(struct cl_peer *p, const char *fmt, va_list ap));
 void cl_destroy(struct cl_tree *t);
 
@@ -55,7 +55,7 @@ void cl_again(struct cl_peer *p);
 
 void cl_help(struct cl_peer *p, int mode);
 
-int cl_visible(struct cl_peer *p, int mode, const struct cl_command *command);
+int cl_visible(struct cl_peer *p, int mode, int modes);
 
 #endif
 
